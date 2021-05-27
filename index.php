a48186588fbc32f37b4e8f266635b50b9660c5a5
karin valcanover <valcanoverkarin@outlook.it>
2021-05-27 16:55:23 +0200

    inizio collegamento info stanze

--- index.php
+++ index.php
@@ -16,14 +16,40 @@
     $sql = "SELECT * FROM `stanze` ";
     $result = $conn->query($sql);
 
+    $rooms = [];
     if ($result && $result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
             //var_dump($row);
-            echo "Stanza N. ". $row['room_number']. " piano: ".$row['floor']. " beds: ".$row['beds']. "<br>";
+            $rooms[] = $row;
         }
     }
+
+    //var_dump($rooms);
 
     $conn->close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista stanze</title>
+</head>
+<body>
+    <h1>Lista stanze</h1>
+
+    <ul>
+        <?php foreach($rooms as $room) { ?>
+            <li>
+                numero stanza: <?php echo $rooms['room_number']; ?> <br>
+                piano: <?php echo $rooms['floor']; ?> <br>
+                <a href="">Vedi info stanza</a>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
